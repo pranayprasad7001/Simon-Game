204351716bc8683486ed4b2f3e7658e2785b2d33
pranayprasad7001 <68220629+pranayprasad7001@users.noreply.github.com>
2021-06-09 16:55:34 +0530

    Made changes to count level early

--- game.js
+++ game.js
@@ -47,17 +47,15 @@ function checkAnswer(currentLevel) {
     }
 }
 
-
 function nextSequence() {
   userClickedPattern = [];
-
+  level++;
   $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  level++;
 }
 
 function animatePress(currentColor) {
